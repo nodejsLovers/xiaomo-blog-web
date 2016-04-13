5e3cde5e0f96006a875fab56743208e221f652e0
xiaomo <83387856@qq.com>
2016-04-13 23:16:25 +0800

    拆分模块常量

--- admin/src/app/app.js
+++ admin/src/app/app.js
@@ -1,21 +1,23 @@
-import angular from 'angular';
-import AppCtrl from './controllers/AppCtrl';
-import '../style/app.css';
+import angular from "angular";
+import AppCtrl from "./controllers/AppCtrl";
+import { MODULE_NAME } from "./constants/module";
+import "../style/app.css";
 
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
+    return {
+        template: require('./app.html'),
+        controller: 'AppCtrl',
+        controllerAs: 'app'
+    }
 };
 
 
-
-const MODULE_NAME = 'app';
-
 angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+    .directive('app', app)
+    .controller('AppCtrl', AppCtrl);
 
+/**
+ * 我在module里面己经 export 出来一个 MODULE_NAME
+ * 但是如果这里去掉默认导出的话 test就跑不通.没太明白原因
+ */
 export default MODULE_NAME;
