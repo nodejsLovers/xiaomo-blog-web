d46c477e93e449637ccab21370419de3d9c3dfbd
xiaomo <83387856@qq.com>
2016-04-20 14:12:24 +0800

    不需要http

--- admin/src/js/controllers.js
+++ admin/src/js/controllers.js
@@ -3,7 +3,7 @@
  * @type {angular.IModule}
  */
 angular.module("myControllerModule", [])
-    .controller('BlogListController', ["$scope", "$http", "getBlogListService", function ($scope, $http, getBlogListService) {
+    .controller('BlogListController', ["$scope", "getBlogListService", function ($scope, getBlogListService) {
             $scope.result = getBlogListService.test;
             console.log($scope.result);
         }]
