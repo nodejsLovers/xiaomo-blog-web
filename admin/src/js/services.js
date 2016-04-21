62942a431d6fc115d80641112ba8c773c7433968
xiaomo <83387856@qq.com>
2016-04-21 14:43:11 +0800

    添加博客service

--- admin/src/js/services.js
+++ admin/src/js/services.js
@@ -1,5 +1,5 @@
 angular.module("myServiceModule", [])
-    .service('getBlogListService', function ($rootScope, $http, $q) {
+    .service('getBlogListService', ['$rootScope', '$http', '$q', function ($rootScope, $http, $q) {
         var result = {};
         result.getUserInfo = function (current) {
             var deferred = $q.defer();
@@ -20,4 +20,27 @@ angular.module("myServiceModule", [])
             return deferred.promise;
         };
         return result;
-    });
+    }]).service('addBlogService', function ($rootScope, $http) {
+    var result = {};
+    result.addBlog = function (blog) {
+        $http({
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            url: $rootScope.$baseUrl + "/admin/blog/add",
+            method: 'POST',
+            dataType: 'json',
+            params: {
+                title: blog.title,
+                summary: blog.summary,
+                content: blog.content,
+                nickName: blog.author,
+                blogType: blog.blogType,
+                tagIds: blog.tagIds
+            }
+        })
+            .success(function () {
+                alert("添加成功！")
+            })
+    };
+    return result;
+
+});
