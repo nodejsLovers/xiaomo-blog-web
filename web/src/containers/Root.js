e800520753883a56d1e93b00cfd89046f9cba42a
xiaomo <83387856@qq.com>
2016-05-19 10:26:08 +0800

    修改一下说明

--- web/src/containers/Root.js
+++ web/src/containers/Root.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    console.log(process.env.NODE_ENV);
+    // 在linux 系统下 vim ~/.bash_profile 添加 export NODE_ENV=production
     module.exports = require('./Root.prod.js');
 } else {
     module.exports = require('./Root.dev.js');
