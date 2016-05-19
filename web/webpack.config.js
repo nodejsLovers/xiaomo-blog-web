9ddc268d6fbdabd9cb57024661121e290e3a9d7e
xiaomo <83387856@qq.com>
2016-05-19 15:30:57 +0800

    去掉说明

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,7 +1,6 @@
 // process.env.NODE_ENV = 'production';
 if (process.env.NODE_ENV === 'production') {
     console.info('当前系统环境是' + process.env.NODE_ENV);
-    // 在linux 系统下 vim ~/.bash_profile 添加 export NODE_ENV=production
     module.exports = require('./config/webpack.prod.js');
 } else {
     console.info('当前系统环境是[测试环境]');
