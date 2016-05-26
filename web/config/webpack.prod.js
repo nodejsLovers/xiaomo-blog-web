cb38c58d795528094e74e7339398c14eeab32484
xiaomo <83387856@qq.com>
2016-05-26 09:54:09 +0800

    去掉说明

--- web/config/webpack.prod.js
+++ web/config/webpack.prod.js
@@ -12,20 +12,6 @@
  * @Description:
  * @Copyright(©) 2015 by xiaomo.
  **/
-/**
- * 把今天最好的表现当作明天最新的起点．．～
- * いま 最高の表現 として 明日最新の始発．．～
- * Today the best performance  as tomorrow newest starter!
- * Created by IntelliJ IDEA.
- *
- * @author: xiaomo
- * @github: https://github.com/qq83387856
- * @email: hupengbest@163.com
- * @QQ_NO: 83387856
- * @Date: 2016/5/16 10:00
- * @Description:
- * @Copyright(©) 2015 by xiaomo.
- **/
 var webpack = require('webpack');
 var path = require('path');
 var htmlWebpackPlugin = require('html-webpack-plugin');
